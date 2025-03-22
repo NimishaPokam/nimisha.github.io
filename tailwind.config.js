822644cbd6b1212d3b711987defd5b339121e2cc
NimishaPokam <nimishapokam@gmail.com>
2025-03-21 20:34:20 -0500

    baseurl

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./content/**/*.{html,md,markdown}",
-    "./layouts/**/*.{html,partial}",
-    "./static/**/*.{html,js}",
-    "./themes/digitalgarden/layouts/**/*.{html,partial}",
-    "./themes/digitalgarden/content/**/*.{html,md,markdown}",
-    "./themes/digitalgarden/static/**/*.{html,js}",
-    "./themes/digitalgarden/layouts/**/*.html",
-    "./themes/digitalgarden/assets/js/*.js"
+    './layouts/**/*.html',
+    './content/**/*.{html,md}',
+    './themes/digitalgarden/layouts/**/*.html',
   ],
-  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
   ],
+  darkMode: 'class'
 }
